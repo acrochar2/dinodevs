f8179a38e3c27d7a876e91cc4ef9127b483a607a
acrochar2 <anacristina.rocha@outlook.com>
2015-02-12 00:34:00 -0600

    Ejercicios
    Yield Return

---
+++ Ejercicios/CapaDatos/Class1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDatos
+{
+    public class Class1
+    {
+
+        public IEnumerable<int> EjercicioUno()
+        {
+            int i = 0;
+            while(true)
+            {
+                yield return i++;
+            }
+        }
+
+        public IEnumerable<int> EjercicioDos()
+        {
+            int i = 0;
+            while (true)
+            {
+                if (i % 2 == 0)
+                {
+                    yield return i;
+                    
+                }
+                i++;
+                
+            }
+        }
+
+        public IEnumerable<int> EjercicioTres()
+        {
+            int i = 0;
+            while (true)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    yield return i;
+                    
+                }
+                i++;
+            }
+
+        }
+
+        public IEnumerable<string> EjercicioCuatro()
+        {
+            int i = 0;
+            while (true)
+            {
+                if (i % 3 == 0)
+                {
+                    yield return "Hola";
+
+                }
+                else if (i % 5 == 0)
+                {
+                    yield return "Mundo";
+
+                }
+                else if (i % 3 == 0 && i % 5 == 0)
+                {
+                    yield return "HolaMundo";
+
+                }
+                else
+                {
+                    yield return i.ToString();
+                }
+                
+                i++;
+            }
+
+        }
+    }
+}
---
+++ Ejercicios/Ejercicios/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CapaDatos;
+
+namespace Ejercicios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 ClaseMetodos = new Class1();
+
+            Console.Write("Elija una opcion\n");
+            Paint(ListaOpciones());
+
+            string opcion = Console.ReadLine();
+            switch (opcion)
+            {
+
+                case "1":
+                    {
+                        Paint(ClaseMetodos.EjercicioUno());
+                        break;
+                    }
+                case "2":
+                    {
+                        Paint(ClaseMetodos.EjercicioDos());
+                        break;
+                    }
+                case "3":
+                    {
+                        Paint(ClaseMetodos.EjercicioTres());
+                        break;
+                    }
+                case "4":
+                    {
+                        Paint(ClaseMetodos.EjercicioCuatro());
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("Opcion No valida");
+                        Console.Read();
+                        break;
+                    }
+                    
+
+        }
+        }
+
+        public static IEnumerable<string> ListaOpciones()
+        {
+            yield return "1 - Devuelve lista infinita de numeros";
+            yield return "2 - Devuelve lista infinita de numeros pares";
+            yield return "3 - Devuelve lista infinita de numeros multiplos de 3 o 5";
+            yield return "4 - Devuelve lista infinita de strings";
+        }
+        
+        public static void Paint(IEnumerable<string> ui)
+        {
+            foreach (var line in ui)
+            {
+                Console.WriteLine(line);
+            }
+        }
+
+        public static void Paint(IEnumerable<int> ui)
+        {
+            foreach (var line in ui)
+            {
+                Console.WriteLine(line);
+            }
+        }
+    }
+}
+
